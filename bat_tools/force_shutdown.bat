8e05e2825823efc9bced5c74e516039d05a9f276
Ivydude76 <115644891+Ivydude76@users.noreply.github.com>
2023-03-27 20:54:46 +0300

    Add files via upload

---
+++ bat_tools/force_shutdown.bat
@@ -0,0 +1,47 @@
+shutdown
+
+shutdown -s
+
+shutdown -r
+
+shutdown -l
+
+shutdown -h
+
+-h
+
+shutdown.exe
+
+shutdown -h
+
+shutdown -i
+
+shutdown -a
+
+-f
+
+-t 4
+
+-t 0
+
+-c <message>
+
+-y
+
+-f
+
+rundll32
+
+/<switch>
+
+-<switch>
+
+/
+
+-f
+
+shutdown -t 0 -r -f
+
+shutdown -t 30 -r
+
+-f
